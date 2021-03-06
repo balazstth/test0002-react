f510afce34a2835927a44c07a333468aa12f082c
Tóth, Balázs Aladár <balazs@aladar.me>
2021-03-07 00:50:49 +0100

    Pathname fix

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function sortFx() {
 // Proved to be more efficient than a hook-based solution in my tests.
 const flagImages = "ad,ae,af,ag,al,am,ar,at,au,az,ba,bb,bd,be,bf,bg,bh,bi,bj,bn,bo,br,bs,bt,bw,by,bz,ca,cd,cf,cg,ch,ci,cl,cm,cn,co,cr,cu,cv,cy,cz,de,dj,dk,dm,do,dz,ec,ee,eg,er,es,et,fi,fj,fm,fr,ga,gb,gd,ge,gh,gm,gn,gq,gr,gt,gw,gy,hn,hr,ht,hu,id,ie,il,in,iq,ir,is,it,jm,jo,jp,ke,kg,kh,ki,km,kn,kp,kr,kw,kz,la,lb,lc,li,lk,lr,ls,lt,lu,lv,ly,ma,mc,md,me,mg,mh,mk,ml,mm,mn,mr,mt,mu,mv,mw,mx,my,mz,na,ne,ng,ni,nl,no,np,nr,nz,om,pa,pe,pg,ph,pk,pl,pt,pw,py,qa,ro,rs,ru,rw,sa,sb,sc,sd,se,sg,si,sk,sl,sm,sn,so,sr,ss,st,sv,sy,sz,td,tg,th,tj,tl,tm,tn,to,tr,tt,tv,tw,tz,ua,ug,us,uy,uz,va,vc,ve,vn,vu,ws,xk,ye,za,zm,zw";
 
-const placeholderImageName = "_placeholder";
+const placeholderImageName = "placeholder";
 
 const flagsLocation = "/test0002-react/flags/";
 
